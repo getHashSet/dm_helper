0cc142eac14dd20cc1a46c951439b39088b24ab5
Chantel Romero <momo@Chantels-MacBook-Pro.local>
2020-12-31 10:25:56 -0700

    updated hp UI and added hp hook

--- client/src/components/_subcomponents/EnemyCard/EnemyCard.jsx
+++ client/src/components/_subcomponents/EnemyCard/EnemyCard.jsx
@@ -6,8 +6,8 @@ import { showToastMenuState, updateToastData } from "../../../redux/actions";
 EnemyCard.defaultProps = {
     enemyName : "Enemy",
     ac : 14,
-    initiative : 1,
-    hp : 10,
+    cr : 1,
+    hitDice : "2d10",
     movement : ["walk", "swim", "fly", "dig"],
     stats : {
         STR : 14,
@@ -33,11 +33,40 @@ EnemyCard.defaultProps = {
 //   COMPONENT   //
 // ============= //
 export default function EnemyCard(props) {
+    // ==================== //
+    //   FAKE CONSTRUCTOR   //
+    // ==================== //
+    const getHp = (dice) => {
+        let totalHp = 0;
+
+        // check if someone put in an bad hit dice value by mistake
+        if (dice.indexOf("d") === -1) {
+            return isNaN(dice) ? totalHp : dice;
+        };
+
+        const diceRolls = dice.split("d"); // "2d10" === ["2", "10"]
+        const numberOfDice = +diceRolls[0]; // (int)3
+        const diceType = +diceRolls[1]; // (int)10
+
+        // roll without con mod?
+        for (let i = 0; i < numberOfDice; i++) {
+            if (i === 0) { // first roll
+                totalHp += diceType;
+            } else { // not first roll
+                totalHp += Math.floor(Math.random() * diceType) + 1;
+            };
+        };
+
+        console.log(totalHp);
+        return totalHp;
+    };
+
     // =================== //
     //   HOOK INTO STATE   //
     // =================== //
     const dispatch = useDispatch(); // used to send data back to redux
-    const [enemyHp, updateenemyHp] = useState(props.hp);
+    const [enemyHp, updateenemyHp] = useState(getHp(props.hitDice));
+    const [maxHp] = useState(enemyHp);
     const [hasAdvantage, updatehasAdvatage] = useState(false);
 
     // ================ //
@@ -114,20 +143,20 @@ export default function EnemyCard(props) {
     const adjustHpButton = (e) => {
         e.preventDefault();
 
+        // heal or damage?
         const type = e.target.getAttribute('name');
         let inputData = e.target.parentElement.children[0].value;
-        console.log(`input data: ${inputData} and its type is ${typeof inputData}`);
-        if ( +inputData <= 0 ) { inputData = 1 };
-        console.log(`input data: ${inputData}`);
-        let newHpTotal = enemyHp;
 
-        console.log("test");
-        console.log(inputData, newHpTotal);
+        // Edgecase user didnt put in a value
+        if ( +inputData <= 0 ) { inputData = 1 };
+
+        // current HP total
+        let newHpTotal = +enemyHp;
 
         switch (type) {
             case "heal":
                 newHpTotal = enemyHp + +inputData;
-                if (newHpTotal > props.hp) {newHpTotal = props.hp};
+                if (newHpTotal > maxHp) {newHpTotal = maxHp};
                 break;
             default:
                 newHpTotal = enemyHp - +inputData;
@@ -139,6 +168,9 @@ export default function EnemyCard(props) {
         updateenemyHp(newHpTotal);
     };
 
+    //   HOOKS AGAIN  //
+    const [initiative, updateinitiative] = useState(rollPlusMod(props.stats.DEX));
+
     // ========== //
     //   RETURN   //
     // ========== //
@@ -148,34 +180,34 @@ export default function EnemyCard(props) {
             {/* Enemy Name */}
             <div className="card_name">
                 <h2>{props.enemyName}</h2>
-                <div className="toggle" hasAdvantage={hasAdvantage} onClick={() => updatehasAdvatage(!hasAdvantage)}></div>
+                <div className="toggle" ></div>
             </div>
 
             {/* AC HP IN */}
             <StyledAcHpIn>
                 <div className="top_section">
                     {/* AC */}
-                    <div className="block">
+                    <div className="block" title="Armor Class">
                         <div className="background"></div>
                         <div className="info">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shield-alt" class="svg-inline--fa fa-shield-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M466.5 83.7l-192-80a48.15 48.15 0 0 0-36.9 0l-192 80C27.7 91.1 16 108.6 16 128c0 198.5 114.5 335.7 221.5 380.3 11.8 4.9 25.1 4.9 36.9 0C360.1 472.6 496 349.3 496 128c0-19.4-11.7-36.9-29.5-44.3zM256.1 446.3l-.1-381 175.9 73.3c-3.3 151.4-82.1 261.1-175.8 307.7z"></path></svg>
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shield-alt" className="svg-inline--fa fa-shield-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M466.5 83.7l-192-80a48.15 48.15 0 0 0-36.9 0l-192 80C27.7 91.1 16 108.6 16 128c0 198.5 114.5 335.7 221.5 380.3 11.8 4.9 25.1 4.9 36.9 0C360.1 472.6 496 349.3 496 128c0-19.4-11.7-36.9-29.5-44.3zM256.1 446.3l-.1-381 175.9 73.3c-3.3 151.4-82.1 261.1-175.8 307.7z"></path></svg>
                             {props.ac}
                         </div>
                     </div>
                     {/* HP */}
-                    <div className="block">
+                    <div className="block" title="Hit Points">
                         <div className="background"></div>
                         <div className="info">
-                            <svg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><title>Heart</title><path d='M256 448a32 32 0 01-18-5.57c-78.59-53.35-112.62-89.93-131.39-112.8-40-48.75-59.15-98.8-58.61-153C48.63 114.52 98.46 64 159.08 64c44.08 0 74.61 24.83 92.39 45.51a6 6 0 009.06 0C278.31 88.81 308.84 64 352.92 64c60.62 0 110.45 50.52 111.08 112.64.54 54.21-18.63 104.26-58.61 153-18.77 22.87-52.8 59.45-131.39 112.8a32 32 0 01-18 5.56z'/></svg>
-                            {enemyHp}
+                            <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'><title>Heart</title><path d='M256 448a32 32 0 01-18-5.57c-78.59-53.35-112.62-89.93-131.39-112.8-40-48.75-59.15-98.8-58.61-153C48.63 114.52 98.46 64 159.08 64c44.08 0 74.61 24.83 92.39 45.51a6 6 0 009.06 0C278.31 88.81 308.84 64 352.92 64c60.62 0 110.45 50.52 111.08 112.64.54 54.21-18.63 104.26-58.61 153-18.77 22.87-52.8 59.45-131.39 112.8a32 32 0 01-18 5.56z'/></svg>
+                            {enemyHp} / {maxHp}
                         </div>
                     </div>
                     {/* IN */}
-                    <div className="block">
+                    <div className="block" title="Initiative Roll">
                         <div className="background"></div>
                         <div className="info">
-                            <svg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><title>Flash</title><path d='M194.82 496a18.36 18.36 0 01-18.1-21.53v-.11L204.83 320H96a16 16 0 01-12.44-26.06L302.73 23a18.45 18.45 0 0132.8 13.71c0 .3-.08.59-.13.89L307.19 192H416a16 16 0 0112.44 26.06L209.24 489a18.45 18.45 0 01-14.42 7z'/></svg>
-                            {props.initiative}
+                            <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'><title>Flash</title><path d='M194.82 496a18.36 18.36 0 01-18.1-21.53v-.11L204.83 320H96a16 16 0 01-12.44-26.06L302.73 23a18.45 18.45 0 0132.8 13.71c0 .3-.08.59-.13.89L307.19 192H416a16 16 0 0112.44 26.06L209.24 489a18.45 18.45 0 01-14.42 7z'/></svg>
+                            {initiative}
                         </div>
                     </div>
                 </div>
@@ -335,6 +367,7 @@ const StyledAcHpIn = styled.section`
             justify-content: center;
             align-items: center;
             text-align: center;
+            user-select: none;
 
             .info {
                 padding: .5em 0;
