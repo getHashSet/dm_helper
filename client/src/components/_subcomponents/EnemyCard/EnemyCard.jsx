b50ca3ab450287b25187b5db6ea78ec5c02f2b7e
Chantel Romero <momo@Chantels-MacBook-Pro.local>
2020-12-31 12:20:47 -0700

    just need to do the math for actions.

--- client/src/components/_subcomponents/EnemyCard/EnemyCard.jsx
+++ client/src/components/_subcomponents/EnemyCard/EnemyCard.jsx
@@ -18,13 +18,27 @@ EnemyCard.defaultProps = {
         CHA : 16
     },
     pageNumber : "Monster Manual",
-    actions : 1,
-    attacks : [
+    actionsNumber : 1,
+    actions : [
         {
-            attack: "bite",
-            bonusToHit : 5,
-            bonusToDamage : 5,
-            info : "",
+            type: "attack", // attack, item, spell
+            actionType: "action", // action, bonus action, passive, reaction.
+            actionName: "Bite",
+            hitMod : 5,
+            damageMod : 3, // number - or - "str", "dex" - or - "finess"
+            damage: "1d6",
+            description: "Attack is +5 to hit to deal 1d6 + 3 damage.",
+            flavorText : "Enemy bites you.",
+            charges : 1,
+        },
+        {
+            type: "item",
+            actionType: "bonus_action",
+            actionName: "Potion",
+            roll: "2d4", // edge case. what if its like 2d4 + 2?
+            diceMod: 2,
+            description: "Drink potion to heal 2d4 + 2 hp.",
+            flavorText : "A potion of red goo that smells tart."
         }
     ]
 }
@@ -57,7 +71,6 @@ export default function EnemyCard(props) {
             };
         };
 
-        console.log(totalHp);
         return totalHp;
     };
 
@@ -74,11 +87,11 @@ export default function EnemyCard(props) {
     // ================ //
     const updateToastMenu = (str) => {
         const html = <StyledToast>
-            <h4>{str}</h4>
+            {str}
         </StyledToast>
         dispatch(showToastMenuState(true)); // redux => state => is it visible "true or false"
         dispatch(updateToastData(html)); // default parent is a div with flex turned on.
-    }
+    };
     
     const rollSavingThrow = (e) => {
         let statMod = 0;
@@ -108,9 +121,9 @@ export default function EnemyCard(props) {
         if (d20 === 20) {savingThrow = "Nat 20"};
         if (d20 === 1) {savingThrow = "Nat 1"};
 
-        console.log(`Sending ${savingThrow} to Toast.`)
+        savingThrow = <h4>{savingThrow}</h4>;
         updateToastMenu(savingThrow);
-    }
+    };
 
     const getStatMod = (stat) => {
         if (stat > 9 && stat < 12) {return};
@@ -122,18 +135,18 @@ export default function EnemyCard(props) {
         if (stat > 11) {
             return `(${(stat - 10) / 2})`;
         }
-    }
+    };
 
     const getMod = (stat) => {
         const uncleanedMod = getStatMod(stat);
         const mod = uncleanedMod.slice(1, uncleanedMod.length - 1); // remove string data
         return +mod;
-    }
+    };
 
     const d20PlusMod = (mod) => {
         const d20 = Math.floor(Math.random() * 20) + 1;
         return d20 + +mod;
-    }
+    };
 
     const rollPlusMod = (stat) => {
         const mod = getMod(stat);
@@ -168,7 +181,38 @@ export default function EnemyCard(props) {
         updateenemyHp(newHpTotal);
     };
 
+    const buildAction = (action) => {
+        if (action.type === null || action.type === undefined) {return};
+
+        // type: "attack", // attack, item, spell
+        const actionType = action.actionType // action, bonus action, passive, reaction.
+        const actionName = action.actionName;
+        const hitMod = action.hitMod;
+        const actiondamageMod = action.actiondamageMod; // number - or - "str", "dex" - or - "finess"
+        const damage = action.damage; // 1d6 
+        const description = action.description;
+        const flavorText = action.flavorText;
+        const charges = action.charges;
+
+        return  <StyledAction onClick={() => updateToastMenu(flavorText)}>
+                    <div className="dice_box">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="dice-d20" className="svg-inline--fa fa-dice-d20 fa-w-15" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512"><path fill="currentColor" d="M106.75 215.06L1.2 370.95c-3.08 5 .1 11.5 5.93 12.14l208.26 22.07-108.64-190.1zM7.41 315.43L82.7 193.08 6.06 147.1c-2.67-1.6-6.06.32-6.06 3.43v162.81c0 4.03 5.29 5.53 7.41 2.09zM18.25 423.6l194.4 87.66c5.3 2.45 11.35-1.43 11.35-7.26v-65.67l-203.55-22.3c-4.45-.5-6.23 5.59-2.2 7.57zm81.22-257.78L179.4 22.88c4.34-7.06-3.59-15.25-10.78-11.14L17.81 110.35c-2.47 1.62-2.39 5.26.13 6.78l81.53 48.69zM240 176h109.21L253.63 7.62C250.5 2.54 245.25 0 240 0s-10.5 2.54-13.63 7.62L130.79 176H240zm233.94-28.9l-76.64 45.99 75.29 122.35c2.11 3.44 7.41 1.94 7.41-2.1V150.53c0-3.11-3.39-5.03-6.06-3.43zm-93.41 18.72l81.53-48.7c2.53-1.52 2.6-5.16.13-6.78l-150.81-98.6c-7.19-4.11-15.12 4.08-10.78 11.14l79.93 142.94zm79.02 250.21L256 438.32v65.67c0 5.84 6.05 9.71 11.35 7.26l194.4-87.66c4.03-1.97 2.25-8.06-2.2-7.56zm-86.3-200.97l-108.63 190.1 208.26-22.07c5.83-.65 9.01-7.14 5.93-12.14L373.25 215.06zM240 208H139.57L240 383.75 340.43 208H240z"></path></svg>
+                    </div>
+                    <div className="info">
+                        <div className="title">
+                            {actionName}
+                        </div>
+                        <div className="body">
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </StyledAction>
+    };
+
+    // ============== //
     //   HOOKS AGAIN  //
+    // ============== //
+    // because of a rookie mistake that I cant be bothered to fix Im just going to put this hook here.
     const [initiative, updateinitiative] = useState(rollPlusMod(props.stats.DEX));
 
     // ========== //
@@ -224,6 +268,7 @@ export default function EnemyCard(props) {
                 </div>
             </StyledAcHpIn>
 
+            {/* STATS */}
             <ul className="stats">
 
                 <li className="class" 
@@ -264,13 +309,18 @@ export default function EnemyCard(props) {
                 </li>
             </ul>
 
+            {/* COMBAT ACTIONS */}
+            <div className="actions">
+                <p>Actions</p>
+                {props.actions.map(action => buildAction(action))}
+            </div>
         </StyledCard>
     )
 }
 
-// ======== //
-//   CARD   //
-// ======== //
+// ========= //
+//   STYLE   //
+// ========= //
 const StyledCard = styled.article`
     width: 30%;
     min-width: 350px;
@@ -282,7 +332,7 @@ const StyledCard = styled.article`
     background-color: #fff;
     border: 2px solid #e84c3b;
     border-radius: .5em;
-    color:#2f3640;
+    color:#2d3436;
 
     .card_name {
         width: 100%;
@@ -340,10 +390,87 @@ const StyledCard = styled.article`
         }
     }
 
+    .actions {
+        border: 1px solid #bdc3c7;
+        border-radius: .5em;
+        background: #fff;
+        overflow-y: scroll;
+        overflow-x: hidden;
+    }
+
     @media (max-width: 768px) {
         width: 100%;
         max-width: 400px;
         min-width: 0px;
+    }
+`;
+
+// ========== //
+//   ACTION   //
+// ========== //
+const StyledAction = styled.div`
+    display: flex;
+    margin: .5em;
+    border-radius: 4px;
+    border: 1px solid #bdc3c7;
+    user-select: none;
+
+    .dice_box {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: .5em;
+        width: 1.2em;
+        overflow: hidden;
+        user-select: none;
+        background-color: #e74c3c;
+        color: #fff;
+
+        svg {
+            width: 1em;
+            height: 1em;
+            max-width: 100%;
+            max-height: 100%;
+        }
+    }
+
+    .info {
+        width: 100%;
+
+        .title {
+            padding: 4px;
+            border-bottom: 1px solid #bdc3c7;
+            font-size: 1.2em;
+            font-weight: 700;
+            color: #fff;
+            background-color: #e74c3c;
+        }
+
+        .body {
+            padding: 4px;
+            font-size: .8em;
+            font-weight: 100;
+        }
+    }
+
+    &:hover {
+        cursor: pointer;
+        border: 1px solid #c0392b;
+        box-shadow: 0 0 1px #c0392b;
+
+        .dice_box {
+            background-color: #c0392b;
+        }
+
+        .info {
+            .title {
+                background-color: #c0392b;
+            }
+        }
+    }
+
+    &:active {
+        transform: translateY(3px);
     }
 `;
 
