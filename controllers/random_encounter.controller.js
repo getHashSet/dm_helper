4c4678e0375ee6ff77f18bbdc6f9ac228fee0eb7
Matthew Carpenter <53285976+getHashSet@users.noreply.github.com>
2021-01-02 16:34:33 -0700

    Update random_encounter.controller.js

--- controllers/random_encounter.controller.js
+++ controllers/random_encounter.controller.js
@@ -38,6 +38,7 @@ router.route("/").post(function (req, res) {
 
   if (req.body.enemies.length === 0){
     devEncounter.encounter = enemyTable[d100];
+    devEncounter.encounter.enemies = [];
     enemies = enemyTable[d100].enemies;
   } else {
     enemies = req.body.enemies;
