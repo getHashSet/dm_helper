6c93e51eb914a9aca044fedbf135a8a7fb7126dd
Chantel Romero <momo@Chantels-MacBook-Pro.local>
2021-01-09 21:50:11 -0700

    updated dice formats.

--- client/src/styles/StyledElements.jsx
+++ client/src/styles/StyledElements.jsx
@@ -191,182 +191,211 @@ export const Toast = styled.article`
 
 //   DICE ROLL   //
 export const DiceRoll = styled.div`
-    text-align: center;
-    padding: .5em .5em 2em .5em;
+  position:relative;
+  text-align: center;
+  padding: .5em .5em 2em .5em;
+  min-width: 256px;
+  max-width: calc(100vw - 3em);
 
-    h3 {
+  h3 {
+      text-align: center;
+      font-size: 2em;
+      padding: 0 0 .5em 0;
+      margin: .3em 0;
+      border-bottom: 1px solid #bdc3c7;
+  }
+
+  h4 {
+      padding: 1em .5em;
+      font-weight: 200;
+      font-size: 3em;
+      font-family: ${props => props.theme.font.title};
+
+      span {
+          font-weight: 900;
+      }
+  }
+
+  h5 {
+    padding: 1em;
+    border-top: 1px solid #bdc3c7;
+    border-bottom: 1px solid #bdc3c7;
+    font-weight: 400;
+    font-style: italic;
+
+    span {
+        font-weight: 600;
+        font-style: normal;
+    }
+
+    &.selectedRoll {
+        opacity: .5;
+    }
+  }
+
+  .heading {
+      width: 100%;
+      padding: .5em 0 .3em 0;
+      font-weight: 400;
+      font-size: .8em;
+      color: #7f8c8d;
+  }
+
+  .roll { //div
+      display: flex;
+      margin: 0;
+      flex-wrap: nowrap;
+      justify-content: center;
+      align-items: center;
+      border-top: 1px solid #bdc3c7;
+
+      p {
+          width: 50%;
+          padding: .3em;
+          display: flex;
+          flex-grow: 1;
+          justify-content: center;
+          align-items: center;
+
+          &:first-child {
+              border-right: 1px solid #bdc3c7;
+              font-weight: 400;
+
+              span {
+                  padding-right: .5em;
+                  font-size: .8em;
+                  font-style: italic;
+                  color: #7f8c8d;
+              }
+          }
+      }
+  }
+
+  .formula {
+      padding: 1em;
+      border: 1px solid #bdc3c7;
+      font-weight: 400;
+      font-style: italic;
+
+      span {
+          font-weight: 600;
+          font-style: normal;
+      }
+
+      &.selectedRoll {
+          opacity: .5;
+      }
+  }
+
+  .hit_dice {
+      &:last-child {
+          border-top: none;
+      }
+  }
+
+  .totals {
+      font-weight: 400;
+      display: flex;
+      flex-wrap: nowrap;
+      justify-content: center;
+      align-items: center;
+      border-radius: 8px 8px 0 0;
+      border: 1px solid #bdc3c7;
+      border-bottom: none;
+
+      p {
+          display: flex;
+          flex-grow: 1;
+          justify-content: center;
+          align-items: center;
+          padding: .5em;
+          margin-bottom: 0;
+          width: 50%;
+
+          span {
+              font-weight: 600;
+              padding: 0 .2em;
+          }
+
+          &:first-child {
+              border-right: 1px solid #bdc3c7;
+          }
+      }
+  }
+
+  .multiattack {
+      font-weight: 400;
+      padding: .5em;
+  }
+
+  .overflow {
+      height: 0px;
+      max-height: 50vh;
+      overflow-y: auto;
+      overflow-x: hidden;
+      padding: 0 .5em;
+      margin: .5em;
+
+      div {
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
         text-align: center;
-        font-size: 2em;
-        padding: 0 0 .5em 0;
-        margin: .3em 0;
-        border-bottom: 1px solid #bdc3c7;
-    }
-
-    h4 {
-        padding: 1em .5em;
-        font-weight: 200;
-        font-size: 3em;
-        font-family: ${props => props.theme.font.title};
-
-        span {
-            font-weight: 900;
-        }
-    }
-
-    .heading {
         width: 100%;
-        padding: .5em 0 .3em 0;
-        font-weight: 400;
-        font-size: .8em;
-        color: #7f8c8d;
-    }
-
-    .roll { //div
-        display: flex;
-        margin: 0;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-items: center;
-        border-top: 1px solid #bdc3c7;
+        border-bottom: 1px solid #bdc3c7;
 
         p {
-            width: 50%;
-            padding: .3em;
-            display: flex;
-            flex-grow: 1;
-            justify-content: center;
-            align-items: center;
-
-            &:first-child {
-                border-right: 1px solid #bdc3c7;
-                font-weight: 400;
-
-                span {
-                    padding-right: .5em;
-                    font-size: .8em;
-                    font-style: italic;
-                    color: #7f8c8d;
-                }
-            }
-        }
-    }
-
-    .formula {
-        padding: 1em;
-        border: 1px solid #bdc3c7;
-        font-weight: 400;
-        font-style: italic;
-
-        span {
-            font-weight: 600;
-            font-style: normal;
+          width: 40%;
+          
+          &:first-child {
+            width: 60%;
+          }
         }
 
-        &.selectedRoll {
-            opacity: .5;
+        .the_roll {
+          text-align: center;
+          border-left: 1px solid #bdc3c7;
         }
-    }
+      }
 
-    .hit_dice {
-        &:last-child {
-            border-top: none;
-        }
-    }
+      span {
+          font-weight: 200;
+          font-style: italic;
+          font-size: .8em;
+          color: #34495e;
+      }
 
-    .totals {
-        font-weight: 400;
-        display: flex;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-items: center;
-        border-radius: 8px 8px 0 0;
-        border: 1px solid #bdc3c7;
-        border-bottom: none;
+      p {
+          display: block;
+          font-weight: 400;
+          padding: .2em;
+          margin: 0;
+          text-align: center;
+      }
 
-        p {
-            display: flex;
-            flex-grow: 1;
-            justify-content: center;
-            align-items: center;
-            padding: .5em;
-            margin-bottom: 0;
-            width: 50%;
+      &.expand {
+          height: auto;
+      }
 
-            span {
-                font-weight: 600;
-                padding: 0 .2em;
-            }
+      &::-webkit-scrollbar {
+          width: 5px;
+          height: 80%;
+          background-color: rgba(255, 255, 255, 0.6);
+      }
+      
+      &::-webkit-scrollbar-track {
+          background-color: #bdc3c7;
+      }
+      
+      &::-webkit-scrollbar-thumb {
+          background-color: #7f8c8d;
+      }
+  }
 
-            &:first-child {
-                border-right: 1px solid #bdc3c7;
-            }
-        }
-    }
-
-    .multiattack {
-        font-weight: 400;
-        padding: .5em;
-    }
-
-    .overflow {
-        height: 0px;
-        max-height: 50vh;
-        overflow-y: auto;
-        overflow-x: hidden;
-        padding: 0 .5;
-        margin: .5;
-
-        div {
-            text-align: center;
-            display: flex;
-            flex-wrap: nowrap;
-            width: 100%;
-            border-bottom: 1px solid #bdc3c7;
-
-            .the_roll {
-                text-align: center;
-                border-left: 1px solid #bdc3c7;
-            }
-        }
-
-        span {
-            font-weight: 200;
-            font-style: italic;
-            font-size: .8em;
-            color: #34495e;
-        }
-
-        p {
-            display: block;
-            font-weight: 400;
-            font-size: .5em;
-            padding: .2em;
-            margin: 0;
-            text-align: center;
-        }
-
-        &.expand {
-            height: auto;
-        }
-
-        &::-webkit-scrollbar {
-            width: 5px;
-            height: 80%;
-            background-color: rgba(255, 255, 255, 0.6);
-        }
-        
-        &::-webkit-scrollbar-track {
-            background-color: #bdc3c7;
-        }
-        
-        &::-webkit-scrollbar-thumb {
-            background-color: #7f8c8d;
-        }
-    }
-
-    @media (max-width: ${props => props.theme.breakpoint.mobile}) {
-        width: calc(100vw - 1em);
-    }
+  @media (max-width: ${props => props.theme.breakpoint.mobile}) {
+      width: calc(100vw - 1em);
+  }
 `;
 
 //   REFRESH BUTTON   //
